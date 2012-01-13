4195f2ee8a9594abdd752905560576a6384634f4
David Faber <dafaber@gmail.com>
2012-01-13 11:45:36 -0800

    Make explicit definitions of nested modules

--- lib/data_table/renderer/csv.rb
+++ lib/data_table/renderer/csv.rb
@@ -1,11 +1,15 @@
 require 'csv'
-module DataTable::Renderer::CSV
-  def to_csv(opts={})
-    yield self
-    CSV.generate do |csv|
-      csv << columns.map(&:label)
-      records.each do |r|
-        csv << columns.map { |c| c.value(r) }
+module DataTable
+  module Renderer
+    module CSV
+      def to_csv(opts={})
+        yield self
+        CSV.generate do |csv|
+          csv << columns.map(&:label)
+          records.each do |r|
+            csv << columns.map { |c| c.value(r) }
+          end
+        end
       end
     end
   end
--- lib/data_table/renderer/html.rb
+++ lib/data_table/renderer/html.rb
@@ -1,9 +1,13 @@
-module DataTable::Renderer::HTML
-  def to_html(opts={})
-    self.html_opts = opts
-    yield self if block_given?
-    @view = ActionView::Base.new
-    @template = Tilt.new(template_path)
-    @template.render @view, locals
+module DataTable
+  module Renderer
+    module HTML
+      def to_html(opts={})
+        self.html_opts = opts
+        yield self if block_given?
+        @view = ActionView::Base.new
+        @template = Tilt.new(template_path)
+        @template.render @view, locals
+      end
+    end
   end
 end
