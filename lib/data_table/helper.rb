1cfc7985d8c092e05f3c86ecad1bd1e866eeeb40
David Faber <dafaber@gmail.com>
2012-01-13 11:58:45 -0800

    try using render template instead of partial

--- lib/data_table/helper.rb
+++ lib/data_table/helper.rb
@@ -12,7 +12,7 @@ module DataTable
       template = opts.delete(:template) || table.template_path
       table.html_opts = opts
       yield table if block_given?
-      render :partial => template, :locals => table.locals
+      render :template => template, :locals => table.locals
     end
     
     def sort_url_for_column(col)
